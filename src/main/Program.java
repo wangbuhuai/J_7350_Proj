51b88a2fa65242ea1fdf8160896d46f4ce25a3ac
Dayu Wang <dwang@stchas.edu>
2022-11-23 08:34:45 -0600

    Initial commit

---
+++ src/main/Program.java
@@ -0,0 +1,12 @@
+// Created by Dayu Wang (dwang@stchas.edu) on 2022-11-23
+
+// Last updated by Dayu Wang (dwang@stchas.edu) on 2022-11-23
+
+
+package main;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
